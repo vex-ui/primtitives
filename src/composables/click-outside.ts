201d188a56fc1706eefb9cd3b0033cc5dd0e6ac9
shareef <creazyhell@gmail.com>
2023-09-16 06:23:12 +0200

    use windowEvent composable

--- src/composables/click-outside.ts
+++ src/composables/click-outside.ts
@@ -1,6 +1,9 @@
 import type { Fn, MaybeRefOrGetter, TemplateRef } from '@/types'
-import { isClient, isIOS, isWatchable, noop, remove } from '@/utils'
+import { isClient, isIOS, isWatchable, noop } from '@/utils'
 import { onScopeDispose, toValue, watch } from 'vue'
+import { useWindowEvent } from './window-event'
+
+type Listener = (e: PointerEvent) => void
 
 interface Options {
   /**
@@ -20,25 +23,30 @@ interface Options {
  */
 export function useClickOutside(source: TemplateRef, cb: Listener, options: Options = {}): Fn {
   if (!isClient) return noop
+  useIosWorkaround()
 
   const { ignore = [], isActive = true } = options
 
   const onClick = (e: PointerEvent): void => {
-    const shouldIgnore = [source, ...toValue(ignore)].some((ref) => {
-      const el = toValue(ref)
-      return el && isElementInsideComposedPath(e, el)
+    const path = e.composedPath()
+    const elements = [source, ...toValue(ignore)]
+
+    const shouldIgnore = elements.some((templateRef) => {
+      const el = toValue(templateRef)
+      return el && path.includes(el)
     })
 
-    if (shouldIgnore) return
-    cb(e)
+    if (!shouldIgnore) {
+      cb(e)
+    }
   }
 
-  let unregister = toValue(isActive) ? registerListener(onClick) : noop
+  let unregister = toValue(isActive) ? useWindowEvent('pointerdown', onClick) : noop
 
   if (isWatchable(isActive)) {
     watch(isActive, (active) => {
       if (active) {
-        unregister = registerListener(onClick)
+        unregister = useWindowEvent('pointerdown', onClick)
       } else {
         unregister()
         unregister = noop
@@ -50,50 +58,6 @@ export function useClickOutside(source: TemplateRef, cb: Listener, options: Opti
   return unregister
 }
 
-//----------------------------------------------------------------------------------------------------
-// 📌 Global Click Event Listener
-//
-// The idea is to have a single shared window listener that automatically removes itself
-// when there are no active listeners in the array, the window listener should loop through
-// and run all the registered listeners.
-//----------------------------------------------------------------------------------------------------
-
-type Listener = (e: PointerEvent) => void
-
-const listeners: Listener[] = []
-let removeGlobalListener = noop
-let isGlobalListenerAttached = false
-
-function registerListener(listener: Listener) {
-  listeners.push(listener)
-
-  if (!isGlobalListenerAttached) {
-    removeGlobalListener = useWindowEventListener((e: PointerEvent) => {
-      listeners.forEach((listener) => listener(e))
-    })
-    isGlobalListenerAttached = true
-  }
-
-  return () => {
-    remove(listeners, listener)
-    if (!listeners.length) {
-      removeGlobalListener()
-      isGlobalListenerAttached = false
-    }
-  }
-}
-
-//----------------------------------------------------------------------------------------------------
-// 📌 utils
-//----------------------------------------------------------------------------------------------------
-
-function useWindowEventListener(listener: Listener) {
-  useIosWorkaround()
-
-  window.addEventListener('pointerdown', listener, { passive: true, capture: true })
-  return () => window.removeEventListener('pointerdown', listener, { capture: true })
-}
-
 // See: https://www.quirksmode.org/blog/archives/2014/02/mouse_event_bub.html
 let isIOSWorkaroundActive = false
 function useIosWorkaround() {
@@ -101,8 +65,4 @@ function useIosWorkaround() {
     isIOSWorkaroundActive = true
     Array.from(document.body.children).forEach((el) => el.addEventListener('click', noop))
   }
-}
-
-function isElementInsideComposedPath(e: PointerEvent, el: HTMLElement) {
-  return e.target === el || e.composedPath().includes(el)
 }
