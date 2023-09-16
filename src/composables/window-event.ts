60334d862d314b9179a449d76094251b2615fa97
shareef <creazyhell@gmail.com>
2023-09-16 06:49:37 +0200

    revert map changes

--- src/composables/window-event.ts
+++ src/composables/window-event.ts
@@ -9,8 +9,10 @@ type SharedEventHandler<T extends WindowEvents> = {
   isAttached: boolean
   remove: () => void
 }
-
-const handlersMap = new Map<WindowEvents, SharedEventHandler<WindowEvents>>()
+type HandlersMap = {
+  [K in WindowEvents]?: SharedEventHandler<K>
+}
+const handlersMap: HandlersMap = {}
 
 export function useWindowEvent<T extends WindowEvents>(eventType: T, listener: Listener<T>): Fn {
   if (!isClient) return noop
@@ -30,38 +32,27 @@ export function useWindowEvent<T extends WindowEvents>(eventType: T, listener: L
   return cleanup
 }
 
-function attachHandler<T extends WindowEvents>(
-  handler: SharedEventHandler<T>,
-  eventType: T
-): SharedEventHandler<T> {
+function attachHandler<T extends WindowEvents>(handler: SharedEventHandler<T>, eventType: T): void {
   if (!handler.isAttached) {
     const listener: Listener<T> = (e) => handler.listeners.forEach((cb) => cb(e))
 
     window.addEventListener(eventType, listener)
+    handler.isAttached = true
 
     handler.remove = () => {
+      if (!handler.isAttached) return
       window.removeEventListener(eventType, listener)
       handler.isAttached = false
     }
-
-    handler.isAttached = true
   }
-
-  return handler
 }
 
 function getHandler<T extends WindowEvents>(eventType: T): SharedEventHandler<T> {
-  if (handlersMap.has(eventType)) {
-    return handlersMap.get(eventType)!
-  }
-
-  return handlersMap
-    .set(eventType, {
-      listeners: [],
-      isAttached: false,
-      remove: noop,
-    })
-    .get(eventType)!
+  return (handlersMap[eventType] ??= {
+    listeners: [],
+    isAttached: false,
+    remove: noop,
+  })
 }
 
 /**
@@ -69,8 +60,8 @@ function getHandler<T extends WindowEvents>(eventType: T): SharedEventHandler<T>
  * this function is here only to support tests
  */
 export const clearMap = () => {
-  for (const [, handler] of handlersMap) {
-    handler.remove()
+  for (const [key, value] of Object.entries(handlersMap)) {
+    value.remove()
+    delete handlersMap[key as WindowEvents]
   }
-  handlersMap.clear()
 }
