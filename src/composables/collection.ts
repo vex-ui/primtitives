57ea2f9e18c7a5db44679789a58e005420a21102
shareef <creazyhell@gmail.com>
2023-09-17 07:50:59 +0200

    refactor to use a class

--- src/composables/collection.ts
+++ src/composables/collection.ts
@@ -1,46 +1,46 @@
 import type { Getter, TemplateRef } from '@/types'
 import { computed, shallowReactive } from 'vue'
 
-type Item<T extends HTMLElement> = {
+export type Item<T extends HTMLElement> = {
   id: string
   templateRef: TemplateRef<T>
   disabled?: Getter<boolean>
 }
 
-export function createCollection<T extends HTMLElement = HTMLElement>(collectionID: string) {
-  const _collection = shallowReactive<Map<TemplateRef<T>, Item<T>>>(new Map())
+export class Collection<T extends HTMLElement = HTMLElement> {
+  #id: string
+  #count: number = 0
+  #collection = shallowReactive<Map<TemplateRef<T>, Item<T>>>(new Map())
 
-  const items = computed(() => [..._collection.values()])
-
-  const elements = computed(() =>
-    [..._collection.keys()].reduce<T[]>((arr, ref) => {
+  items = computed(() => [...this.#collection.values()])
+  elements = computed(() =>
+    [...this.#collection.keys()].reduce<T[]>((arr, ref) => {
       const el = ref.value
       el != null && arr.push(el)
       return arr
     }, [])
   )
 
-  let count = 0
-  const getID = () => `${collectionID}-${count++}`
+  constructor(id: string) {
+    this.#id = id
+  }
 
-  const add = (templateRef: TemplateRef<T>, disabled?: Getter<boolean>) => {
-    const id = getID()
+  private generateID(): string {
+    return `${this.#id}-${this.#count++}`
+  }
+
+  add(templateRef: TemplateRef<T>, disabled?: Getter<boolean>): Item<T> {
+    const id = this.generateID()
     const item = { id, templateRef, disabled }
-    _collection.set(templateRef, item)
+    this.#collection.set(templateRef, item)
     return item
   }
 
-  const remove = (templateRef: TemplateRef<T>) => {
-    _collection.delete(templateRef)
-  }
-
-  return {
-    items,
-    elements,
-    add,
-    remove,
+  remove(templateRef: TemplateRef<T>): void {
+    this.#collection.delete(templateRef)
   }
 }
 
 // FIXME: remove this
+export const createCollection = () => {}
 export const useCollection = () => {}
