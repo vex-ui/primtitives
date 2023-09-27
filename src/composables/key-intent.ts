1428374855cb2f320fa05546e8bb97c25b713c46
shareef <creazyhell@gmail.com>
2023-09-27 18:38:20 +0200

    refactor(useKeyIntent): move dir to useKeyIntent scope so it can be auto cleaned up

--- src/composables/key-intent.ts
+++ src/composables/key-intent.ts
@@ -1,24 +1,36 @@
-import type { Fn, Getter, KeyIntent, NavigationKey, Orientation, TemplateRef } from '@/types'
+import type {
+  Fn,
+  KeyIntent,
+  MaybeRefOrGetter,
+  NavigationKey,
+  Orientation,
+  TemplateRef,
+} from '@/types'
+import { toValue } from 'vue'
 import { useEventListener, useTextDirection } from '.'
+import type { Dir } from './text-direction'
 
 type Listener = (e: KeyboardEvent, intent: KeyIntent, key: NavigationKey) => void
 
 interface Options {
-  orientation?: Getter<Orientation>
+  orientation?: MaybeRefOrGetter<Orientation>
 }
 
 export function useKeyIntent(target: TemplateRef, listener: Listener, options: Options = {}): Fn {
+  const { orientation = 'vertical' } = options
+  const dir = useTextDirection()
+
   return useEventListener(target, 'keydown', (e: KeyboardEvent) => {
     const key = e.key
     if (!isNavigationKey(key)) return
 
-    const intent = getKeyIntent(key, options.orientation?.())
+    const intent = getKeyIntent(key, dir.value, toValue(orientation))
     listener(e, intent, key)
   })
 }
 
-export function getKeyIntent(key: NavigationKey, orientation: Orientation = 'vertical'): KeyIntent {
-  switch (getDirectionAwareKey(key)) {
+export function getKeyIntent(key: NavigationKey, dir: Dir, orientation: Orientation): KeyIntent {
+  switch (getDirectionAwareKey(key, dir)) {
     case 'ArrowDown':
       if (orientation === 'vertical') return 'next'
       return 'show'
@@ -43,10 +55,9 @@ export function getKeyIntent(key: NavigationKey, orientation: Orientation = 'ver
   }
 }
 
-const dir = useTextDirection()
-
-function getDirectionAwareKey(key: NavigationKey) {
-  if (dir.value !== 'rtl') return key
+// FIXME: handle dir === 'auto' | undefined cases
+function getDirectionAwareKey(key: NavigationKey, dir: Dir) {
+  if (dir !== 'rtl') return key
   return key === 'ArrowLeft' ? 'ArrowRight' : key === 'ArrowRight' ? 'ArrowLeft' : key
 }
 
