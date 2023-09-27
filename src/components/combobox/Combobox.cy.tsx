d389ec5f446ba26999585ae52576127a4326c49d
shareef <creazyhell@gmail.com>
2023-09-27 09:01:51 +0200

    chore(tests): added more tests to combobox

--- src/components/combobox/Combobox.cy.tsx
+++ src/components/combobox/Combobox.cy.tsx
@@ -1,17 +1,28 @@
-import { Combobox, ComboboxDropdown, ComboboxOption, ComboboxTrigger } from './Combobox'
+import { defineComponent } from 'vue'
+import { ComboboxDropdown, ComboboxOption, ComboboxTrigger, ComboboxListbox } from './Combobox'
+import { useCombobox } from './ComboboxContext'
 import { mount } from 'cypress/vue'
 
 describe('Combobox', () => {
-  const component = () => (
-    <Combobox>
-      <ComboboxTrigger></ComboboxTrigger>
-      <ComboboxDropdown>
-        <ComboboxOption>item 1</ComboboxOption>
-        <ComboboxOption>item 2</ComboboxOption>
-        <ComboboxOption>item 3</ComboboxOption>
-      </ComboboxDropdown>
-    </Combobox>
-  )
+  const Combobox = defineComponent({
+    setup(p, { slots }) {
+      const { isDropdownVisible } = useCombobox()
+      return () => (
+        <>
+          <ComboboxTrigger></ComboboxTrigger>
+          <ComboboxDropdown>
+            <ComboboxListbox v-show={isDropdownVisible.value}>
+              <ComboboxOption value="item 1">item 1</ComboboxOption>
+              <ComboboxOption value="item 2">item 2</ComboboxOption>
+              <ComboboxOption value="item 3">item 3</ComboboxOption>
+            </ComboboxListbox>
+          </ComboboxDropdown>
+        </>
+      )
+    },
+  })
+
+  const component = Combobox
 
   beforeEach(() => {
     mount(component)
@@ -19,5 +30,25 @@ describe('Combobox', () => {
 
   it('renders without crashing', () => {
     cy.get('[role=combobox]').should('exist')
+  })
+
+  it('has correct aria attributes', () => {
+    cy.get('[role=combobox]').as('combobox')
+    cy.get('[role=listbox]').as('listbox')
+
+    cy.get('@listbox').invoke('attr', 'id').should('have.length.above', 0)
+    cy.get('@combobox').invoke('attr', 'id').should('have.length.above', 0)
+
+    cy.get('@listbox')
+      .invoke('attr', 'id')
+      .then((listboxID) => {
+        cy.get('@combobox').should('have.attr', 'aria-controls', listboxID)
+      })
+
+    cy.get('@combobox')
+      .invoke('attr', 'id')
+      .then((comboboxID) => {
+        cy.get('@listbox').should('have.attr', 'aria-labelledby', comboboxID)
+      })
   })
 })
